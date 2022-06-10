ac61c5a6efc2cfb7b152becafe53e5a7ea736ec1
IgorJLR <89113480+IgorJLR@users.noreply.github.com>
2022-06-09 21:07:24 -0300

    marcador de pesquisa

--- src/pages/searchResults/index.js
+++ src/pages/searchResults/index.js
@@ -3,10 +3,18 @@ import React from "react";
 import MapResults from '../../components/mapResults';
 import { Container } from "./style";
 
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
+import "leaflet-defaulticon-compatibility";
+
 export default function SearchResults() {
   return (
     <Container>
       <h1> DA ZAPXAP PESQUISA</h1>
+      <Marker position={[40.8054, -74.0241]} draggable={true} animate={true}>
+       <Popup>Hey ! I live here</Popup>
+      </Marker>
       <MapResults />
     </Container>
   );
