25d47d38263ec975f817aabac76808324232e111
IgorJLR <89113480+IgorJLR@users.noreply.github.com>
2022-06-09 21:13:24 -0300

    teste

--- src/pages/searchResults/index.js
+++ src/pages/searchResults/index.js
@@ -3,18 +3,12 @@ import React from "react";
 import MapResults from '../../components/mapResults';
 import { Container } from "./style";
 
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import "leaflet/dist/leaflet.css";
-import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
-import "leaflet-defaulticon-compatibility";
+
 
 export default function SearchResults() {
   return (
     <Container>
-      <h1> DA ZAPXAP PESQUISA</h1>
-      <Marker position={[40.8054, -74.0241]} draggable={true} animate={true}>
-       <Popup>Hey ! I live here</Popup>
-      </Marker>
+      <h1> DA ZOp PESQUISA</h1>
       <MapResults />
     </Container>
   );
